dae2ae4401564042484c26625b25e5e98604b014
journey12581 <journey12581@gmail.com>
2022-05-10 16:19:47 +0800

    第一次提交

---
+++ T4Test2/TextTemplate1.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTextTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world hhhbbb");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ T4TestHelper2/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TestHelper2
+{
+    public class Generator
+    {
+        public static string PrintSomething()
+        {
+            return "Console.WriteLine(\"hello world hhhbbb\");";
+        }
+    }
+}
